fffb5085b0b86f7ee0be640a6bb16ff61e202b0f
Abdelrahman Badawi <168288640+Abdo-Badawi@users.noreply.github.com>
2025-05-10 00:04:33 +0300

    Update DBController.php

--- Controllers/DBController.php
+++ Controllers/DBController.php
@@ -5,7 +5,7 @@ class DBController {
     private $host = "localhost";
     private $user = "root";
     private $password = "";
-    private $database = "homestay3";
+    private $database = "homestay";
     
    
     // Open database connection
